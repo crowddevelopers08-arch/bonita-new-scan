4e9f1df1be71e900596dd6b8d48a396f2b8fca9d
fenil matt fenil matt <ffenilmatt@gmail.com>
2026-07-18 19:11:56 +0530

    meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '828427233146959');
+            fbq('init', '1348530381088676');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -45,7 +45,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=828427233146959&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1348530381088676&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
